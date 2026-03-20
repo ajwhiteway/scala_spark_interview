db5906a2dcd0f3d86a83711afce8dd4a6635111d
Aaron Whiteway <ajwhiteway@gmail.com>
2026-03-20 11:36:25 -0300

    update

--- src/main/scala/examples/AdditivePrimes/AdditivePrimes.scala
+++ src/main/scala/examples/AdditivePrimes/AdditivePrimes.scala
@@ -9,20 +9,6 @@ object AdditivePrimes {
 //     // primeDigitSumPrimes(5) => List(2, 3, 5, 7, 23)
 //     return
 
-    def primeDigitSumPrimes(n: Int): List[Int] = {
-    def isPrime(x: Int): Boolean =
-        x > 1 && (2 to math.sqrt(x).toInt).forall(x % _ != 0)
-
-    def digitSum(x: Int): Int =
-        x.toString.map(_.asDigit).sum
-
-    LazyList.from(2)
-        .filter(isPrime)
-        .filter(x => isPrime(digitSum(x)))
-        .take(n)
-        .toList
-    }
-
     def main(args: Array[String]): Unit = {
 
         print("How many primes? ")
